9201489adbe4620b7d3431c4e41aa50e77d84027
jerry <jerry.zhang@boldseas.com>
2017-03-28 15:54:06 +0800

    return new Object with every single properties

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,7 +2,10 @@
  * Created by jerry on 3/28/17.
  */
 const toggleTodo = (todo) => {
-    todo.completed = !todo.completed;
-    return todo;
+    return {
+        id: todo.id,
+        text: todo.text,
+        completed: !todo.completed
+    };
 }
 export default toggleTodo;
