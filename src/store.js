0a11928caf547dde40028a3e79d35d5685159ada
jerry <jerry.zhang@boldseas.com>
2017-03-28 19:06:05 +0800

    add redux devtools enhancers

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 /**
  * Created by jerry on 3/28/17.
  */
-import { createStore } from 'redux';
+import { createStore , compose} from 'redux';
 import todoApp from './reducer/index';
 
-const store = createStore(todoApp);
+const enhancers = compose(
+    window.devToolsExtension?window.devToolsExtension(): f=>f
+);
+const store = createStore(todoApp,enhancers);
 
 
 export default store;
