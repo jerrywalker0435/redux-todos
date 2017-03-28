85685157837f03c7a20ee102ff26aa80b59285cd
jerry <jerry.zhang@boldseas.com>
2017-03-28 15:57:06 +0800

    toggleTodo: spread operator version

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,8 +2,10 @@
  * Created by jerry on 3/28/17.
  */
 const toggleTodo = (todo) => {
-    return Object.assign({},todo,
-        {completed: !todo.completed}
-    );
+    return{
+        ...todo,
+        completed: !todo.completed
+        }
+
 }
 export default toggleTodo;
