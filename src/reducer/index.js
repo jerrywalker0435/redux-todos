dbb06097d9ae74c1ba242fe81e118bfd64919714
jerry <jerry.zhang@boldseas.com>
2017-03-28 15:55:40 +0800

    toggleTodo Object.assign version

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,10 +2,8 @@
  * Created by jerry on 3/28/17.
  */
 const toggleTodo = (todo) => {
-    return {
-        id: todo.id,
-        text: todo.text,
-        completed: !todo.completed
-    };
+    return Object.assign({},todo,
+        {completed: !todo.completed}
+    );
 }
 export default toggleTodo;
