826ac45507ea84618dd362701457c2f0e2cd7d92
jerry.zhang <jerry.zhang@boldseas.com>
2017-04-06 15:25:51 +0800

    use Provider provide by react-redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider}from 'react-redux';
 import TodoApp from './components/TodoApp';
-import Provider from './Provider';
 import './static/css/index.css';
 import store from './store';
 ReactDOM.render(
     <Provider store={store}>
-    <TodoApp/>
+        <TodoApp/>
     </Provider>,
     document.getElementById('root')
 );
