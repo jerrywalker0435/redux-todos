6fe80a6e03f66a9d55e4a16395cddb5ec8e7871a
jerry.zhang <jerry.zhang@boldseas.com>
2017-04-06 15:31:30 +0800

    use connect to generate Container Component

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,41 +1,27 @@
 /**
  * Created by jerry on 4/6/17.
  */
-import React from 'react';
 import TodoList from './TodoList';
 import { getVisibleTodos } from '../reducer/filter';
-class VisibleTodoList extends React.Component {
-    componentDidMount(){
-        const { store } = this.context;
-        this.unsubscribe = store.subscribe(()=>this.forceUpdate());
-    }
-
-    componentWillUnmount(){
-        this.unsubscribe();
-    }
-    render(){
-        const { store } = this.context;
-
-        const state = store.getState();
+import { connect } from 'react-redux';
 
 
-        return (
-            <TodoList todos={
-                getVisibleTodos(state.todos, state.visibilityFilter)
-            }
-                onTodoClick={
-                    id => store.dispatch({
-                        type: 'TOGGLE_TODO',
-                        id
-                    })
-                }
-            />
-        )
+const mapStateToProps = (state) => {
+    return {
+        todos: getVisibleTodos(state.todos, state.visibilityFilter),
     }
 }
 
-VisibleTodoList.contextTypes = {
-    store: React.PropTypes.object
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onTodoClick: id =>
+            dispatch({
+                type: 'TOGGLE_TODO',
+                id
+            })
+    }
 }
+const VisibleTodoList = connect(mapStateToProps,mapDispatchToProps)(TodoList);
 
 export default VisibleTodoList;
